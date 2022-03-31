39de65eb2d93cf8a3b5dced5aeb9d5da70416ecd
Malik <malik.h@webdevpro.net>
2022-03-31 15:38:26 +0200

    yes !!

--- jour4-todo/src/App.vue
+++ jour4-todo/src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <h1>Todos List</h1>
-  <Form />
+  <Form @add="addTodo" />
   <List :datas="liste"  />
 </template>
 <script>
 import Form from "./components/Form.vue"
 import List from "./components/List.vue"
-import {ref } from "vue"
+import { ref } from "vue"
 export default {
   setup(){
     let liste = ref([{id : 1 , nom : "tache1" , status : false}])
 
+    function addTodo(todo){
+      liste.value.push(todo)
+    }
+
     return {
-      liste
+      liste,
+      addTodo
     }
   },
   components : {Form, List}
+  // 15h25 bon café @ toute suite !!!
 }
 </script>
--- jour4-todo/src/components/Form.vue
+++ jour4-todo/src/components/Form.vue
@@ -1,3 +1,24 @@
 <template>
     <p>je suis le formulaire</p>
+    <button @click="ajouter">ajouter une nouvelle tâche </button>
 </template>
+<script>
+export default {
+    setup(props, context){
+        function ajouter(){
+            context.emit('add', {id : 2 , nom : "tache 2" , status : false})
+        }
+        return {
+            ajouter
+        }
+    }
+}
+</script>
+<!--
+cas pratique 
+dans le composant Form => dans la partie template 
+créer un formulaire avec input de type texte + input de type submit
+créer un variable réactive qui permet de récupérer la valeur saisit dans le champ text
+créer une méthode qui va être déclenchée lorsque l'on soumet le formulaire
+en utilisant la nouvelle API  
+-->
