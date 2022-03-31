62a38561c7d796816f4c47fbf46ec0f2ffa91bff
Malik <malik.h@webdevpro.net>
2022-03-31 12:33:16 +0200

    valeur par défaut dans un slot

--- jour4-tp/src/components/Article.vue
+++ jour4-tp/src/components/Article.vue
@@ -1,5 +1,7 @@
 <template>
     <article>
-        <slot></slot><!-- permet de récupérer le contenu de la balise parent -->
+        <slot>valeur par défaut</slot><!-- permet de récupérer le contenu de la balise parent
+        possible de remplir slot avec du html => fallback si pas de html de l'appel du composant dans le parent 
+         -->
     </article>
 </template>
